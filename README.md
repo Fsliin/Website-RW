64ea22ca2a5b847d6153232af596fa4b09cd7d77
Faisal Abdallah <abdallahfaisal5@gmail.com>
2026-06-24 17:05:10 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+
+  # Neighborhood Community Landing Page
+
+  This is a code bundle for Neighborhood Community Landing Page. The original project is available at https://www.figma.com/design/wDuhknUunmUarnDG6StqKE/Neighborhood-Community-Landing-Page.
+
+  ## Running the code
+
+  Run `npm i` to install the dependencies.
+
+  Run `npm run dev` to start the development server.
+  
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+
+function figmaAssetResolver() {
+  return {
+    name: 'figma-asset-resolver',
+    resolveId(id) {
+      if (id.startsWith('figma:asset/')) {
+        const filename = id.replace('figma:asset/', '')
+        return path.resolve(__dirname, 'src/assets', filename)
+      }
+    },
+  }
+}
+
+export default defineConfig({
+  plugins: [
+    figmaAssetResolver(), // jika fungsi ini dipakai, hapus tanda komen // di depan
+    react(),
+    tailwindcss(),
+  ],
+  // 👇 INI BAGIAN YANG KITA TAMBAHKAN 👇
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: true
+  },
+  // 👆 ============================== 👆
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  assetsInclude: ['**/*.svg', '**/*.csv'],
+})
